5c32f35c8f812dbb270aa7df742c98a61b78c605
vidyamay <vidyamay@gmail.com>
2015-05-12 14:24:46 -0700

    test

--- wearableConnect/src/com/wc/core/ws/SetHealthData.java
+++ wearableConnect/src/com/wc/core/ws/SetHealthData.java
@@ -3,10 +3,10 @@ package com.wc.core.ws;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
-@Path("/")
+@Path("wc")
 public class SetHealthData {
 
-    @Path("/setHealthInfo")
+    @Path("setHealthInfo")
     @Consumes(MediaType.APPLICATION_JSON)
     @POST
     public String setHealthInfo() {
@@ -14,8 +14,9 @@ public class SetHealthData {
         return "<html><body><p>Name=Vidya</p></body></html>";
     }
     
-    @Path("/getHealthInfo")
+    @Path("getHealthInfo")
     @GET
+    @Produces(MediaType.TEXT_HTML)
     public String getHealthInfo() {
 
         return "<html><body><p>Name=Vidya</p></body></html>";
