e25d172efe08be5ba5f33f77443bfc6d0e92d503
vidyamay <vidyamay@gmail.com>
2015-05-12 15:44:45 -0700

    json input changes

--- wearableConnect/src/com/wc/core/ws/SetHealthData.java
+++ wearableConnect/src/com/wc/core/ws/SetHealthData.java
@@ -1,7 +1,14 @@
 package com.wc.core.ws;
 
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 @Path("wc")
 public class SetHealthData {
@@ -9,9 +16,19 @@ public class SetHealthData {
     @Path("setHealthInfo")
     @Consumes(MediaType.APPLICATION_JSON)
     @POST
-    public String setHealthInfo() {
-
-        return "<html><body><p>Name=Vidya</p></body></html>";
+    public String setHealthInfo(JSONObject inputJObj) {
+        String userId = null;
+        try {
+            userId = (String) inputJObj.get("userId");       
+            System.out.println("userId>> " + userId);
+            
+            
+        } catch (JSONException e) {
+            System.out.println("Parsing error, wrong input!!");
+            e.printStackTrace();
+        }
+       
+        return "<html><body><p>userId: " + userId + "</p></body></html>";
     }
     
     @Path("getHealthInfo")
